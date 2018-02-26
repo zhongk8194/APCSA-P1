0aac3b3f929df285d8fe05305a15415b7e9aa0f3
zhongk8194 <36008055+zhongk8194@users.noreply.github.com>
2018-02-26 11:27:49 -0800

    Update Triples.java

--- Unit 7/src/Triples.java
+++ Unit 7/src/Triples.java
@@ -29,33 +29,13 @@ public class Triples
 
 	}
 	
-	/**public String getTriples(int a, int b, int c)
-	{
-		String output = "";
-		for (a = 1; a <= number; a++)
-		{
-			for (b = 1; b <= number; b++)
-			{
-				for (c = 1; c <= number; c++)
-				{
-					if ((a * a) + (b * b) == (c * c)){
-						if ((a % 2 != 0) && (b % 2 == 0) && (c % 2 != 0) || (a % 2 == 0) && (b % 2 != 0) && (c % 2 != 0)){
-							output = a + "" + b + "" + c;
-						}
-					}
-				}
-			}
-		}
-		return output;
-	}**/
-	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = -1;
 		for (int i = 2; i <= a; i++)
 		{
 			if (a % i == 0 && b % i == 0 && c % i == 0)
-				return 0
+				return 0;
 		}
 
 		return 1;
@@ -70,14 +50,14 @@ public class Triples
 			{
 				for (int k = j + 1; k <= number; k++)
 				{
-					if ((i * i) + (j* j) = (k * k)) && ((i % 2 == 1 && j % 2 == 0 && k % 2 == 1) || (i % 2 == 0 && j % 2 == 1 && k % 2 == 1)) && (greatestCommonFactor(i, j, k) == 1))
+					if ((i * i) + (j* j) == (k * k)) 
+						if ((greatestCommonFactor(i, j, k) == 1))
 						output += i + " " + j + " " + k + "\n";
-				}
 		    
-					}
-					
 				}
+					
 			}
+					
 		}
 		return output + "\n";
 	}
